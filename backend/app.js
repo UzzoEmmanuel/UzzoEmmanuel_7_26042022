55132f2e23c9eba44cf5bc604b800f30751ee6dc
Uzzo Emmanuel <lublulibellule@gmail.com>
2022-04-26 11:46:28 +0200

    3_création server.js et app.js

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+//const path = require("path");
+
+require("dotenv").config();
+
+//const userRoutes = require("./routes/user");
+
+const app = express();
+
+helmet({
+  crossOriginResourcePolicy: false,
+});
+
+//-----------------------------------------------------------------------------------------------
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(express.json());
+
+//app.use("/images", express.static(path.join(__dirname, "images")));
+//app.use("/api/auth", userRoutes);
+//-----------------------------------------------------------------------------------------------
+module.exports = app;
